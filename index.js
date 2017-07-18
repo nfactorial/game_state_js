a55b5c2df20f21fce2b5fa5ff2190456404f6f63
nfactorial <nfactorial@btopenworld.com>
2017-07-18 10:23:38 +0100

    Added simple event provider implementation to the state tree api.

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const parameters = require('./lib/parameters');
 parameters.registerParams();
 
 module.exports = {
-    Parameters: NodeParam,
-    GameSystem: require('./lib/game_system'),
     EventGameSystem: require('./lib/game_system/event_game_system.js'),
+    EventProvider: require('./lib/event_provider'),
+    UpdateArgs: require('./lib/update_args'),
+    GameSystem: require('./lib/game_system'),
     StateTree: require('./lib/state_tree'),
     InitArgs: require('./lib/init_args'),
-    UpdateArgs: require('./lib/update_args')
+    Parameters: NodeParam
 };
